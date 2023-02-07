926ab98d2bfcef84afe95263ab86486bc10b1b01
Polina <106979919+kollarbone@users.noreply.github.com>
2023-02-07 15:02:10 +0400

    tttt

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="hello">
+    <h3>Ticker</h3>
     <input
       v-model="ticker"
-      id="walet"
-      name="walet"
+      v-on:keydown.enter="add"
+      id="wallet"
+      name="wallet"
       type="text"
       placeholder="For example DOGE"
     />
+    <button v-on:click="add">Add</button>
   </div>
 </template>
 
@@ -19,25 +22,50 @@ export default {
   data() {
     return {
       ticker: "",
+      tickers: ["1", "2", "3"],
     };
+  },
+  methods: {
+    add(evt) {
+      alert(this.ticker);
+    },
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.hello {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
 h3 {
-  margin: 40px 0 0;
+  margin: 30px 0 10px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+input {
+  font-size: 20px;
+  border: none;
+  border-bottom: 2px solid rgba(57, 61, 90, 0.5);
+  outline: none;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+input::placeholder {
+  color: rgba(57, 61, 90, 0.5);
 }
-a {
-  color: #42b983;
+button {
+  cursor: pointer;
+  margin: 10px 0 10px;
+  border-radius: 15px;
+  border: none;
+  background-color: rgba(57, 61, 90, 0.5);
+  color: whitesmoke;
+  font-size: 17px;
+  padding: 10px;
+  width: 20%;
+}
+button:hover {
+  background-color: whitesmoke;
+  color: rgba(57, 61, 90, 0.5);
+  border: 1px solid rgba(57, 61, 90, 0.5);
 }
 </style>
